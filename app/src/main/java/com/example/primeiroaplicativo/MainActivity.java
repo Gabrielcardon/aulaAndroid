7d61eb78dca12d452c089c926ad935ed1b4dcb57
Gabrielcardon <gcardon13@gmail.com>
2022-08-17 20:10:49 -0300

    segundo commit

--- app/src/main/java/com/example/primeiroaplicativo/MainActivity.java
+++ app/src/main/java/com/example/primeiroaplicativo/MainActivity.java
@@ -7,10 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView2;
     private Button button;
+    private ArrayList<String> phrases = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         textView2 = findViewById(R.id.textView2);
         button = findViewById(R.id.button);
+        phrases.add("Comprar pastel para o Professor");
+        phrases.add("Comprar pizza para o Professor");
+        phrases.add("Comprar nota para o Professor");
 
 
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView2.setText("MUITO MASSA MEU PRIMEIRO PROJETO");
+
+                Random random = new Random();
+                int position = random.nextInt(phrases.size());
+                String phrase = phrases.get(position);
+                textView2.setText(phrase);
             }
         };
         button.setOnClickListener(listener);
